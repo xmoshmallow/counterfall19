13d45c77593394fc305ba1b3917e67ec780750a9
Koki Johnson <koki.johson@gmail.com>
2019-09-18 18:55:25 -0600

    jsreact notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { value: 0 }
+  //   this.inc = this.inc.bind(this)
+  //   this.dec = this.dec.bind(this)
+  // }
+  state = { value: 0 }
+
+  inc = () => {
+    // asynchronous 
+    // this.setState({ value: this.state.value + 1 })
+    this.setState( (state) => {
+      return { value: state.value + 1}
+    })
+  }
+
+  dec = () => {
+    // this.setState({ value: this.state.value - 1})
+    this.setState( (state) => {
+      return { value: state.value - 1}
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='colored'>Counter Example</h1>
+        <p>{ this.state.value }</p>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    )
+  }
 }
 
+// React.createElement(h1, counter)React.createElement(h1, counter)
+// React.createElement(div, counter)
+
 export default App;
+
+
+
+
+
+
+
+
